c4c62c2cc93348f92ae3e9a13fd9c8c69ecb99fe
ashwini <ashwini1065@gmail.com>
2021-05-09 06:27:10 +0000

    type casting

--- src/oopsConcepts/ClassA.java
+++ src/oopsConcepts/ClassA.java
@@ -7,6 +7,21 @@ public class ClassA {
 	protected String OrgName;
 	int age;
 	
+	public ClassA() {
+		
+		System.out.println("Inside Constructor");
+	}
+	
+	public ClassA(int val1,String val2,String val3) {
+		
+		System.out.println("Inside param Constructor");
+	    Salary = val1;
+	    Name = val2;
+	    OrgName = val3;
+	    
+		
+	}
+	
 	public void Display() {
 		
 		System.out.println("Name of the emp :" + Name);
--- src/oopsConcepts/RunnerClass.java
+++ src/oopsConcepts/RunnerClass.java
@@ -5,12 +5,12 @@ public class RunnerClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ClassA Emp1 = new ClassA();
+		ClassA Emp1 = new ClassA(1000,"Ashwini","lg");
 		
-		Emp1.Name = "Ashwini";
+		/*Emp1.Name = "Ashwini";
 		Emp1.OrgName = "LG Soft India";
 		Emp1.Salary = 10000;
-	    Emp1.age = 34;
+	    Emp1.age = 34;*/
 		
 		Emp1.Display();
 		
